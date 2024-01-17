c42ba54f97d66c35d43f76f1145840c3ef186a55
Patricio9991 <patriciopucheta99@gmail.com>
2024-01-16 22:51:50 -0300

    cambios responsive

--- src/componentes/InfoCh/infoCh.js
+++ src/componentes/InfoCh/infoCh.js
@@ -7,11 +7,11 @@ export default function InfoCh({status,species,origin,gender,setShow}){
         <div >
             <div className="d-flex flex-row">
             
-                <ul className="list-group ps-2">
-                    <li className=" li list-group-item">Character Status {status} </li>
-                    <li className=" li list-group-item"><p>Species</p>{species}</li>
-                    <li className=" li list-group-item"><p>Origin</p>{origin}</li>
-                    <li className=" li list-group-item"><p>Gender</p>{gender}</li>
+                <ul className="list-group">
+                    <li className=" li list-group-item"><p>Character Status: {status}</p></li>
+                    <li className=" li list-group-item"><p>Species:</p>{species}</li>
+                    <li className=" li list-group-item"><p>Origin:</p>{origin}</li>
+                    <li className=" li list-group-item"><p>Gender:</p>{gender}</li>
                     
                 </ul>
 
